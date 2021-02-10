756f0003676a0eb6057d824e6fb561243c9f7ad6
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-02-10 14:53:22 +0330

    Now, it is more readable

--- engine.py
+++ engine.py
@@ -1,36 +1,53 @@
 import csv 
 from pydub import AudioSegment 
 
-csvdata = open("sounds/sounds.csv")
-reader = csv.reader(csvdata)
-next(reader)
+class TTSEngine:
+    def __init__(csv_file, input_text):
+        self.csv_file = csv_file 
+        self.input_text = input_text
+    
+    def data_preparation(self):
+        csvdata = open(self.csv_file)
+        reader = csv.reader(csvdata)
+        next(reader)
 
-data = {}
+        data = {}
 
-for row in reader:
-    data[row[0]] = row[1].replace(' ', '')
+        for row in reader:
+            data[row[0]] = row[1].replace(' ', '')
 
-data_keys = list(data.keys())
-data_values = list(data.values())
+        return data
 
-text = "من مهندس کامپیوتر هستم"
-text = text.split(" ")
+    def make_sounds(self):
+        data = self.data_preparation
+        data_keys = list(data.keys())
+        data_values = list(data.values())
 
-output_sentence = []
+        text = self.input_text.split(" ")
 
-for word in text:
-    if word in data.values():
-        sound_file = data_values.index(word)
-        print(data_keys[sound_file])
-        temp_sound = AudioSegment.from_file(f'sounds/{data_keys[sound_file]}', format="wav")
-        output_sentence.append(temp_sound)
+        output_sentence = []
+
+        for word in text:
+            if word in data.values():
+                sound_file = data_values.index(word)
+                print(data_keys[sound_file])
+                temp_sound = AudioSegment.from_file(f'sounds/{data_keys[sound_file]}', format="wav")
+                output_sentence.append(temp_sound)
 
 
-base_sound = output_sentence[0]
-output_sentence.pop(0)
+        base_sound = output_sentence[0]
+        output_sentence.pop(0)
 
-for wave_file in output_sentence:
-    print(wave_file)
-    base_sound = base_sound.append(wave_file, crossfade=150)
+        for wave_file in output_sentence:
+            print(wave_file)
+            base_sound = base_sound.append(wave_file, crossfade=150)
 
-base_sound.export("out.wav", format="wav")
+        base_sound.export("temp.wav", format="wav")
+        
+
+
+
+
+
+
+
