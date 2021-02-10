ea999338f7b3b035d0f4ecbb719cd6fa42e2e54c
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-02-10 14:58:06 +0330

    Now it has an infinite input prompt

--- engine.py
+++ engine.py
@@ -2,9 +2,8 @@ import csv
 from pydub import AudioSegment 
 
 class TTSEngine:
-    def __init__(csv_file, input_text):
+    def __init__(self, csv_file):
         self.csv_file = csv_file 
-        self.input_text = input_text
     
     def data_preparation(self):
         csvdata = open(self.csv_file)
@@ -18,19 +17,18 @@ class TTSEngine:
 
         return data
 
-    def make_sounds(self):
-        data = self.data_preparation
+    def make_sounds(self, input_text):
+        data = self.data_preparation()
         data_keys = list(data.keys())
         data_values = list(data.values())
 
-        text = self.input_text.split(" ")
+        text = input_text.split(" ")
 
         output_sentence = []
 
         for word in text:
             if word in data.values():
                 sound_file = data_values.index(word)
-                print(data_keys[sound_file])
                 temp_sound = AudioSegment.from_file(f'sounds/{data_keys[sound_file]}', format="wav")
                 output_sentence.append(temp_sound)
 
@@ -39,12 +37,15 @@ class TTSEngine:
         output_sentence.pop(0)
 
         for wave_file in output_sentence:
-            print(wave_file)
             base_sound = base_sound.append(wave_file, crossfade=150)
 
         base_sound.export("temp.wav", format="wav")
         
-
+if __name__ == '__main__':
+    engine = TTSEngine("sounds/sounds.csv")
+    while True:
+        input_text = input("Type a Persian string: ")
+        engine.make_sounds(input_text)
 
 
 
